8b7903a44a82f11d0f5ea08ea5e18dec9b46677b
Eric <Eric@xiiluo.com>
2022-07-04 18:05:12 +0800

    update

--- index.tsx
+++ index.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import { render } from "react-dom";
 
 // 源文件
-// import NoticeBar from "./src/NoticeBar";
+import NoticeBar from "./src/NoticeBar";
 
 // 测试打包后的组件
-import NoticeBar from "./dist/index.esm.js";
-import "./dist/index.esm.css";
-
-const App = () => <NoticeBar />;
+// import NoticeBar from "./dist/index.esm.js";
+// import "./dist/index.esm.css";
+const list: string[] = [
+  "谁念西风独自凉",
+  "萧萧黄叶闭疏窗萧萧黄叶闭疏窗萧萧黄叶闭疏窗萧萧黄叶闭疏窗",
+  "沉思往事立残阳沉思往事立残阳沉思往事立残阳沉思往事立残阳",
+  "被酒莫惊春睡重",
+  "赌书消得泼茶香",
+  "当时只道是寻常"
+];
+const App = () => <NoticeBar message={list} duration={10}/>;
 render(<App />, document.getElementById("root"));
--- src/NoticeBar.tsx
+++ src/NoticeBar.tsx
@@ -1,42 +1,57 @@
 import React, { useEffect, useRef, useState } from "react";
 import style from "./NoticeBar.module.scss";
 
-function NoticeBar(props: any): JSX.Element {
-	const [message] = useState(["12121", "wewqewqeqw", "fdsfdsfsdf", "hhgfhgfhgfhgf", "12121"]);
+interface IProps {
+	message: string[];
+	duration?: number;
+  newLine?: boolean;
+}
+
+const ellipsis = {
+	whiteSpace: "nowrap",
+	overflow: "hidden",
+	textOverflow: "ellipsis"
+};
+function NoticeBar(props: IProps): JSX.Element {
 	const noticeMessageRef = useRef<HTMLUListElement>(null);
 
 	useEffect(() => {
+		const len = props.message.length;
 		const wrapNode = noticeMessageRef.current;
 		const distance = wrapNode!.offsetHeight - 30;
-		noticeMessageRef.current!.style.transitionDuration = `5s`;
+		noticeMessageRef.current!.style.transitionDuration = `${props.duration || len}s`;
 		noticeMessageRef.current!.style.transform = `translateY(${-distance}px)`;
 	}, []);
 	const noticeTransitionEndHandler = () => {
+		const len = props.message.length;
 		const wrapNode = noticeMessageRef.current;
 		const distance = wrapNode!.offsetHeight - 30;
 		noticeMessageRef.current!.style.transitionDuration = "0s";
 		noticeMessageRef.current!.style.transform = `translateY(0)`;
 		setTimeout(() => {
-			noticeMessageRef.current!.style.transitionDuration = `5s`;
+			noticeMessageRef.current!.style.transitionDuration = `${props.duration || len}s`;
 			noticeMessageRef.current!.style.transform = `translateY(${-distance}px)`;
 		}, 0);
 	};
 
 	return (
 		<div className={style["notice-bar"]}>
-			<div className={style["notice-wrap"]}>
-				<img src={require("./notice.png").default} alt="" />
-				<div className={style["tip-content"]}>
-					<ul
-						className={style["tip"]}
-						ref={noticeMessageRef}
-						onTransitionEnd={noticeTransitionEndHandler}
-					>
-						{message.map<JSX.Element>((item: string, index: number) => {
-							return <li key={index}>{item}</li>;
-						})}
-					</ul>
-				</div>
+			<img src={require("./notice.png").default} alt="" />
+			<div className={style["tip-content"]}>
+				<ul
+					className={style["tip"]}
+					ref={noticeMessageRef}
+					onTransitionEnd={noticeTransitionEndHandler}
+				>
+					{props.message.map<JSX.Element>((item: string, index: number) => {
+						return (
+							<li style={{...ellipsis}} key={index}>
+								{item}
+							</li>
+						);
+					})}
+					{props.message.length ? <li>{props.message[0]}</li> : null}
+				</ul>
 			</div>
 		</div>
 	);
