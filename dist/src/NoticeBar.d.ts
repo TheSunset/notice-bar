d6de3f10ab9eb028548ade54af82d3264b2d8187
sunset <570752795@qq.com>
2022-07-04 22:39:18 +0800

    publish

--- dist/src/NoticeBar.d.ts
+++ dist/src/NoticeBar.d.ts
@@ -1,2 +1,8 @@
-declare function NoticeBar(props: any): JSX.Element;
+/// <reference types="react" />
+interface IProps {
+    message: string[];
+    duration?: number;
+    newLine?: boolean;
+}
+declare function NoticeBar(props: IProps): JSX.Element;
 export default NoticeBar;
